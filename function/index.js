9bd6bb7fdd0524ff888500f5c60fa73443a70bfb
Xiaomo <Xiaomo@20mo.cn>
2025-11-17 17:02:34 +0800

    feat: info function

--- function/index.js
+++ function/index.js
@@ -4,6 +4,29 @@ const html = `<!DOCTYPE html>
 </body>`
 
 async function handleRequest(request) {
+    const url = new URL(request.url)
+
+    if (url.pathname === "/info") {
+        const headersObj = {}
+        for (const [key, value] of request.headers.entries()) {
+            headersObj[key] = value
+        }
+
+        const info = {
+            method: request.method,
+            url: request.url,
+            pathname: url.pathname,
+            searchParams: Object.fromEntries(url.searchParams.entries()),
+            headers: headersObj,
+        }
+
+        return new Response(JSON.stringify(info, null, 2), {
+            headers: {
+                "content-type": "application/json; charset=UTF-8",
+            },
+        })
+    }
+
     return new Response(html, {
         headers: {
             "content-type": "text/html;charset=UTF-8",
